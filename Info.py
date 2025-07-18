a58320693d10aed2e633acac50b3d4f0a32722bb
Harshk09876 <harshk06121@gmail.com>
2025-07-18 08:29:44 +0530

    Add files via upload

---
+++ Info.py
@@ -0,0 +1,22 @@
+#Twilio
+
+account_sid = 'AC09b8977887902202678d2754e5fbc4d1'
+auth_token = '9a9070369594bd4263db3b1318e5bc5a'
+twilionumber = '+919259073043'
+twiliosmsnumber = '+919259073043'
+
+#Telegram Bot Token
+API_TOKEN = "7729772878:AAElbpzU7bw0xd0Bl1ANquHZtY6lZq-bMN0" 
+
+#Host URL
+callurl = 'https://03f3-196-191-60-243.ngrok.io' 
+twiliosmsurl = 'https://03f3-196-191-60-243.ngrok.io/sms'
+
+
+
+
+
+
+
+
+
